d7433ca7b5a790f2661cbb1730dc1e5cfb9e14ad
Octane Silvio <ruslangibadullin70@gmail.com>
2022-10-08 18:59:00 +0500

    init commit

---
+++ app/src/main/java/com/octaneocatane/services/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.octaneocatane.services
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.octaneocatane.services.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
